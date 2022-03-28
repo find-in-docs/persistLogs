523ee5e2b7483004b2e5897fdfbd56fad6c1aa32
samirgadkari <samirgadkari@users.noreply.github.com>
2022-03-28 15:36:40 -0700

    Moved code from cli/cmd/serve.go to pkg/logs

--- cli/cmd/serve.go
+++ cli/cmd/serve.go
@@ -1,28 +1,9 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-	"regexp"
-	"time"
-
-	"github.com/samirgadkari/persist/pkg/config"
-	"github.com/samirgadkari/persist/pkg/data"
-	"github.com/samirgadkari/sidecar/pkg/client"
-	pb "github.com/samirgadkari/sidecar/protos/v1/messages"
+	"github.com/samirgadkari/persist/pkg/logs"
 	"github.com/spf13/cobra"
-	"google.golang.org/protobuf/types/known/durationpb"
 )
-
-const (
-	allTopicsRecvChanSize = 32
-)
-
-func formatMsg(msg *string, re *regexp.Regexp) *string {
-
-	result := re.ReplaceAllString(*msg, "")
-	return &result
-}
 
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
@@ -31,69 +12,7 @@ var serveCmd = &cobra.Command{
 	Long: `Start the persistence service. This service will get all messages from
 the message queue and write them into a database.`,
 	Run: func(cmd *cobra.Command, args []string) {
-
-		config.Load()
-
-		// Setup database
-		db, err := data.DBConnect()
-		if err != nil {
-			return
-		}
-
-		tableName := "persist"
-		err = db.CreateTable(tableName)
-		if err != nil {
-			return
-		}
-
-		sidecar := client.InitSidecar(tableName, nil)
-		if sidecar == nil {
-			fmt.Printf("Error initializing sidecar - Exiting\n")
-			os.Exit(-1)
-		}
-
-		topic := "search.*.v1"
-
-		msgStrRegex := regexp.MustCompile(`\\+?\"|\\+?n|\\+?t`)
-
-		go func() {
-			if err = sidecar.ProcessSubMsgs(topic, allTopicsRecvChanSize, func(m *pb.SubTopicResponse) {
-
-				msg := fmt.Sprintf("Received from sidecar:\n\t%s", m.String())
-				msg2 := formatMsg(&msg, msgStrRegex)
-				fmt.Printf("%s\n", *msg2)
-
-				db.StoreData(m.Header, msg2, tableName)
-			}); err != nil {
-				fmt.Printf("Error processing subscription messages:\n\ttopic: %s\n\terr: %v\n",
-					topic, err)
-			}
-		}()
-
-		sidecar.Logger.Log("Persist sending log message test: %s\n", "search.log.v1")
-		time.Sleep(3 * time.Second)
-
-		var retryNum uint32 = 4
-		retryDelayDuration, err := time.ParseDuration("2s")
-		if err != nil {
-			fmt.Printf("Error creating Golang time duration.\nerr: %v\n", err)
-			os.Exit(-1)
-		}
-		retryDelay := durationpb.New(retryDelayDuration)
-
-		err = sidecar.Pub("search.data.v1", []byte("test pub message"),
-			&pb.RetryBehavior{
-				RetryNum:   &retryNum,
-				RetryDelay: retryDelay,
-			},
-		)
-		if err != nil {
-			fmt.Printf("Error publishing message.\n\terr: %v\n", err)
-			os.Exit(-1)
-		}
-
-		sidecar.Unsub(topic)
-		select {} // This will wait forever
+		logs.PersistLogs()
 	},
 }
 
---
+++ pkg/logs/logs.go
@@ -0,0 +1,90 @@
+package logs
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"time"
+
+	"github.com/samirgadkari/persist/pkg/config"
+	"github.com/samirgadkari/persist/pkg/data"
+	"github.com/samirgadkari/sidecar/pkg/client"
+	pb "github.com/samirgadkari/sidecar/protos/v1/messages"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+const (
+	allTopicsRecvChanSize = 32
+)
+
+func formatMsg(msg *string, re *regexp.Regexp) *string {
+
+	result := re.ReplaceAllString(*msg, "")
+	return &result
+}
+
+func PersistLogs() {
+
+	config.Load()
+
+	// Setup database
+	db, err := data.DBConnect()
+	if err != nil {
+		return
+	}
+
+	tableName := "persist"
+	err = db.CreateTable(tableName)
+	if err != nil {
+		return
+	}
+
+	sidecar := client.InitSidecar(tableName, nil)
+	if sidecar == nil {
+		fmt.Printf("Error initializing sidecar - Exiting\n")
+		os.Exit(-1)
+	}
+
+	topic := "search.*.v1"
+
+	msgStrRegex := regexp.MustCompile(`\\+?\"|\\+?n|\\+?t`)
+
+	go func() {
+		if err = sidecar.ProcessSubMsgs(topic, allTopicsRecvChanSize, func(m *pb.SubTopicResponse) {
+
+			msg := fmt.Sprintf("Received from sidecar:\n\t%s", m.String())
+			msg2 := formatMsg(&msg, msgStrRegex)
+			fmt.Printf("%s\n", *msg2)
+
+			db.StoreData(m.Header, msg2, tableName)
+		}); err != nil {
+			fmt.Printf("Error processing subscription messages:\n\ttopic: %s\n\terr: %v\n",
+				topic, err)
+		}
+	}()
+
+	sidecar.Logger.Log("Persist sending log message test: %s\n", "search.log.v1")
+	time.Sleep(3 * time.Second)
+
+	var retryNum uint32 = 4
+	retryDelayDuration, err := time.ParseDuration("2s")
+	if err != nil {
+		fmt.Printf("Error creating Golang time duration.\nerr: %v\n", err)
+		os.Exit(-1)
+	}
+	retryDelay := durationpb.New(retryDelayDuration)
+
+	err = sidecar.Pub("search.data.v1", []byte("test pub message"),
+		&pb.RetryBehavior{
+			RetryNum:   &retryNum,
+			RetryDelay: retryDelay,
+		},
+	)
+	if err != nil {
+		fmt.Printf("Error publishing message.\n\terr: %v\n", err)
+		os.Exit(-1)
+	}
+
+	sidecar.Unsub(topic)
+	select {} // This will wait forever
+}
